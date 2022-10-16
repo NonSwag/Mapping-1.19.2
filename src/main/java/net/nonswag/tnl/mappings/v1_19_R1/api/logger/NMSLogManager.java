18f54857d0119d3348bdec2766c86a1e4d3fe645
NonSwag <mrminecraft00@gmail.com>
2022-10-16 02:50:27 +0200

    removed comment

--- src/main/java/net/nonswag/tnl/mappings/v1_19_R1/api/logger/NMSLogManager.java
+++ src/main/java/net/nonswag/tnl/mappings/v1_19_R1/api/logger/NMSLogManager.java
@@ -17,7 +17,6 @@ public class NMSLogManager extends LogManager {
         try {
             ((Logger) org.apache.logging.log4j.LogManager.getRootLogger()).getAppenders().values().forEach(appender -> {
                 if (!(appender instanceof AbstractFilterable filter)) return;
-                // if (appender.getName().equalsIgnoreCase("TerminalConsole"))
                 filter.addFilter(LogRewriter.getInstance());
                 filter.addFilter(Filter.getInstance());
             });
